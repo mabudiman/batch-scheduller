ad70794d7be310398aca814637f806ab91d34d4f
mabudiman <ma48ud2@gmail.com>
2017-08-12 21:19:17 +0700

    dummy data, meeting, person, jadwal kesibukan

--- src/batch/scheduller/Hungarian.java
+++ src/batch/scheduller/Hungarian.java
@@ -15,7 +15,32 @@ public class Hungarian {
     private int resources;
     private int jobs;
     
-    Hungarian(){
-        
+    Hungarian(int[][] bobot, int resources, int jobs){
+        bobot = new int[resources][jobs];
+        this.bobot = bobot;
+        this.resources = resources;
+        this.jobs = jobs;
+    }
+    
+    void setBobot(int[][] bobotbaru){
+        this.bobot = bobotbaru;
+    }
+    
+    void setResources(int resourcesBaru){
+        this.resources = resourcesBaru;
+    }
+    
+    void setJobs(int jobsbaru){
+        this.jobs = jobsbaru;
+    }
+    
+    public void printBobot()
+    {
+        for(int i = 0; i < this.resources; i++){
+            for(int j = 0; j < this.jobs; j++){
+                System.out.print(bobot[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
